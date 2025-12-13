8422245ef0c2ec102a4cc4bab239a35f1ac7f0de
mixikq <mixq.contact@gmail.com>
2025-12-13 19:39:53 +0300

    Added validation error printing when loggin in, signing up

--- CoffeeDiary/coffee/views.py
+++ CoffeeDiary/coffee/views.py
@@ -14,7 +14,7 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('login')
+            return redirect('home')
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
